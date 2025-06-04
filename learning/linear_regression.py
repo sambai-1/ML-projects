4d71300e53fbf251194b7585ab56e7aee72cf4fb
Sam Bai <sam.bai@berkeley.edu>
2025-06-04 15:58:52 -0700

    learn start

---
+++ learning/linear_regression.py
@@ -0,0 +1,17 @@
+import numpy as np
+from numpy.typing import NDArray
+
+class Solution:
+    
+    def get_model_prediction(self, X: NDArray[np.float64], weights: NDArray[np.float64]) -> NDArray[np.float64]:
+        # X is an Nx3 NumPy array
+        # weights is a 3x1 NumPy array
+        # HINT: np.matmul() will be useful
+        # return np.round(your_answer, 5)
+
+
+    def get_error(self, model_prediction: NDArray[np.float64], ground_truth: NDArray[np.float64]) -> float:
+        # model_prediction is an Nx1 NumPy array
+        # ground_truth is an Nx1 NumPy array
+        # HINT: np.mean(), np.square() will be useful
+        # return round(your_answer, 5)
