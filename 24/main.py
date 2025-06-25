9523534200754427a0d68e8fb63f81fd24f62289
sam <sam.bai@berkeley.edu>
2025-06-25 10:26:06 -0700

    plate recognition done

--- 24/main.py
+++ 24/main.py
@@ -1,7 +1,10 @@
+import pathlib
 import torch
 from torchvision.models.detection import fasterrcnn_resnet50_fpn, FasterRCNN_ResNet50_FPN_Weights
 from torchvision import transforms
 from PIL import Image
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 import sys
 
 DEVICE = torch.device("cpu")
@@ -24,29 +27,31 @@ def process_image(image_path):
     ])
     return transform(image).to(DEVICE)
 
-def largest_bbox(plate):
-    boxes = plate['boxes']
-    areas = (boxes[:, 2] - boxes[:, 0]) * (boxes[:, 3] - boxes[:, 1])
-    max_idx = areas.argmax().item()
-    return boxes[max_idx].tolist()
+def plot_image(image_path, bbox):
+    image = Image.open(image_path).convert("RGB")
+    fig, ax = plt.subplots()
+    ax.imshow(image)
+    rectangle = patches.Rectangle((bbox[0], bbox[1]), bbox[2] - bbox[0], bbox[3] - bbox[1], linewidth = 2, edgecolor="tab:red")
+    ax.add_patch(rectangle)
+    ax.set_axis_off()
+    plt.show()  
+    
+def max_bbox(plate):
+    def area(t):
+        x1, y1, x2, y2 = t.tolist()
+        return (x2 - x1) * (y2 - y1)
+    
+    print(max(plate['boxes'], key = lambda x: area(x)).tolist())
+    
+print("Exists:", pathlib.Path("plate_model.pth").exists()) #path just refuses to exist
+image_path = "main_test_image.jpg"
+models = load_models()
+plate_model = models[0]
+image = process_image(image_path)
 
-def run(image_path):
-    models = load_models()
-    plate_model = models[0]
-    image = process_image(image_path)
-
-    with torch.no_grad():
-        plate_output = plate_model([image])[0]
-    if len(plate_output['boxes']) == 0:
-        print("No plate in picture")
-        return
-    else:
-        max_bbox = largest_bbox(plate_output)
-        print("Bounding box with max area:", max_bbox)
+with torch.no_grad():
+    plate_output = plate_model([image])[0]
+bbox = max_bbox(plate_output)
+print("Bounding box with max area:", max_bbox)
 
 
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: python predict_max_bbox.py <image_path>")
-    else:
-        run(sys.argv[1])
