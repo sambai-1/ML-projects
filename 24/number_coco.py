d0029410d05851dcd02c94e53229483c8991c916
sam <sam.bai@berkeley.edu>
2025-06-27 13:10:16 -0700

    why zeros'

--- 24/number_coco.py
+++ 24/number_coco.py
@@ -1,67 +1,73 @@
+from collections import defaultdict
 import json
 import os
 from PIL import Image
 import torch
-from torch.utils.data import Dataset
+from torchvision.datasets import CocoDetection
 
-class license_coco(Dataset):
-    """
-    COCO but returns data in Faster-R-CNN / RetinaNet-style
-    (image  → Tensor[C,H,W], target → dict w/ boxes & labels).
-    """
-
+class license_coco(CocoDetection):
     def __init__(self, root, ann_file, transforms=None):
-        """
-        root       : directory that holds all images
-        ann_file   : path to _annotations.coco.json
-        transforms : any torchvision / albumentations transform pipeline
-        """
         self.root = root
         self.transforms = transforms
 
-        # ---- load JSON & build look-ups ----
-        coco = json.load(open(ann_file))
-        self.images   = {img["id"]: img for img in coco["images"]}
-        self.cats     = {cat["id"]: cat["name"] for cat in coco["categories"]}
+        #connect annotations to images
+        json_file = json.load(open(ann_file))
+        self.images = {image["id"]: image for image in json_file["images"]}
+        self.labels = {labels["id"]: labels["name"] for labels in json_file["categories"]}
 
-        # map image_id → list[anno]
-        self.annos_per_img = {}
-        for ann in coco["annotations"]:
-            self.annos_per_img.setdefault(ann["image_id"], []).append(ann)
+        self.annotations = defaultdict(list)
+        for item in json_file["annotations"]:
+            self.annotations[item["image_id"]].append(item)
 
-        # keep a list of image_ids so __getitem__ has a stable index
-        self.ids = sorted(self.images.keys())
-
+        self.index = sorted(self.images.keys())
+    
     def __len__(self):
-        return len(self.ids)
+        return len(self.index)
 
-    def __getitem__(self, idx):
-        img_id = self.ids[idx]
-        info   = self.images[img_id]
-        path   = os.path.join(self.root, info["file_name"])
-
-        # -------- load & transform the image --------
-        img = Image.open(path).convert("RGB")
+    def __getitem__(self, index):
+        image_id = self.index[index]
+        image_data = self.images[image_id]
+        image_path = os.path.join(self.root, image_data["file_name"].replace("\\", "/"))
+        print(f"Opening: {image_path}")
+        img = Image.open(image_path).convert("RGB")
         if self.transforms:
             img = self.transforms(img)
 
-        # -------- build target dict --------
-        annos = self.annos_per_img.get(img_id, [])
+        annotations = self.annotations.get(image_id, [])
         boxes   = []
         labels  = []
-        for a in annos:
-            x, y, w, h = a["bbox"]
-            boxes.append([x, y, x + w, y + h])       # to [x1,y1,x2,y2]
-            labels.append(a["category_id"])
+        for charactor in annotations:
+            if charactor["category_id"] == 17:
+                continue
+            x, y, w, h = charactor["bbox"]
+            boxes.append([x, y, x + w, y + h])
+            labels.append(charactor["category_id"])
 
         target = {
-            "boxes":  torch.as_tensor(boxes, dtype=torch.float32),
-            "labels": torch.as_tensor(labels, dtype=torch.int64),
-            "image_id": torch.tensor([img_id])
+            "boxes":  torch.as_tensor(boxes),
+            "labels": torch.as_tensor(labels),
+            "image_id": torch.tensor([image_id])
         }
 
-        return img, target
+        return img, target 
 
-def collate_fn(batch):
-    images, targets = zip(*batch)   # tuple-of-lists → two lists
-    return list(images), list(targets)
+def license_collate(batch):
+    images, targets = zip(*batch)
+    print(images, targets)
+
+    images = torch.stack(images)
+
+    # Flatten all target sequences and record their lengths
+    flat_labels = []
+    target_lengths = []
+
+    for t in targets:
+        label = t["labels"]
+        flat_labels.extend(label.tolist())
+        target_lengths.append(len(label))
+
+    print(flat_labels)
+    flat_targets = torch.tensor(flat_labels)
+    target_lengths = torch.tensor(target_lengths)
+
+    return images, flat_targets, target_lengths
